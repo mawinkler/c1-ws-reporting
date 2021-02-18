c639644b81d3092328f1035cb4336db8b73a699a
Markus Winkler <winkler.info@icloud.com>
2021-02-18 17:23:32 +0100

    reorg

--- host_protection_report/host_protection_report.py
+++ host_protection_report/host_protection_report.py
@@ -1,13 +1,14 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import ssl
-ssl._create_default_https_context = ssl._create_unverified_context
+# ssl._create_default_https_context = ssl._create_unverified_context
 
 import requests
 from requests import Session
 import sys
 import json
-# import xmltodict
+import xmltodict
 import suds.client
+import pprint
 import pickle
 import os.path
 from datetime import datetime
@@ -18,30 +19,25 @@ from zeep.transports import Transport
 from zeep import helpers
 
 
-###
-# CURRENTLY BROKEN
-###
+pp = pprint.PrettyPrinter()
 
 def soap_auth(client, tenant, username, password):
     return client.service.authenticateTenant(tenantName=tenant, username=username, password=password)
 
+def logout(client, sID):
+    client.service.endSession(sID)
+    return True
+
 # Ignoring Timezones, but that should not matter since the interesting info
 # is the duration.
 # No exception handling
-def deep_security_usage_query(dsm_url, dsm_user, dsm_password, dsm_tenant, dsm_tenant_id, timespan_from, timespan_to):
-    # dsm = suds.client.Client('{0}/webservice/Manager?WSDL'.format(dsm_url))
-    session = Session()
-    session.verify = True
-    transport = Transport(session=session, timeout=1800)
-    dsm = Client('{0}/webservice/Manager?WSDL'.format(dsm_url), transport=transport)
-    factory = dsm.type_factory('ns0')
-    sID = ""
-    # if (dsm_tenant):
-    #     sID = dsm.service.authenticateTenant(dsm_tenant, dsm_user, dsm_password)
-    # else:
-    # sID = dsm.service.authenticate(dsm_user, dsm_password)
-    sID = soap_auth(dsm, "Trend Micro Bocking", dsm_user, dsm_password)
+def deep_security_usage_query(client, dsm_url, dsm_user, dsm_password, dsm_tenant, dsm_tenant_id, timespan_from, timespan_to):
+
+    sID = soap_auth(client, "Trend Micro Bocking", dsm_user, dsm_password)
     tID = dsm_tenant_id
+
+    print("sID: {}".format(sID))
+    print("tID: {}".format(tID))
 
     today = datetime.now()
 
@@ -55,13 +51,18 @@ def deep_security_usage_query(dsm_url, dsm_user, dsm_password, dsm_tenant, dsm_t
 
     print("ID,Hostname,IP,Instance Type,Protection Start Date,Protection Stop Date,Duration,AM,WRS,FW,DPI,IM,LI,AC,SAP")
     try:
-        url = dsm_url
+        url = "https://" + dsm_url
         url+= "/rest/monitoring/usages/hosts/protection?tID="+str(tID)+"&sID="
         url+= sID
         url+= "&from="+timespan_from+"&to="+timespan_to
         data = { }
         post_header = { "Content-type": "application/json",
                         "api-version": "v1"}
+
+        print(url)
+        pp.pprint(requests.get(url,
+                                                headers=post_header,
+                                                verify=False).content)
         response = xmltodict.parse(requests.get(url,
                                                 data=json.dumps(data),
                                                 headers=post_header,
@@ -105,23 +106,31 @@ def deep_security_usage_query(dsm_url, dsm_user, dsm_password, dsm_tenant, dsm_t
                                             moduleUsageList))
 
     finally:
-        dsm.service.endSession(sID)
+        logout(client, sID)
 
 def main():
 
     with open("config.yml", "r") as ymlfile:
         cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
     
-    dsm_url = cfg["deepsecurity"]["server"]
+    host = cfg["deepsecurity"]["server"]
     dsm_user = cfg["deepsecurity"]["username"]
     dsm_password = cfg["deepsecurity"]["password"]
-    dsm_tenant = cfg["deepsecurity"]["tenant"]
+    dsm_tenant = cfg["deepsecurity"]["tenant_id"]
     dsm_token = cfg["deepsecurity"]["token"]
     timespan_from = cfg["deepsecurity"]["timespan_from"]
     timespan_to = cfg["deepsecurity"]["timespan_to"]
 
+    session = Session()
+    session.verify = True
+    transport = Transport(session=session, timeout=1800)
+    url = "https://{0}/webservice/Manager?WSDL".format(host)
+    client = Client(url, transport=transport)
+    factory = client.type_factory("ns0")
+
+    print("authenticated")
     # deep_security_usage_query(dsm_url, dsm_user, dsm_password, dsm_tenant, dsm_tenant_id, timespan_from, timespan_to)
-    deep_security_usage_query(dsm_url, dsm_user, dsm_password, 0, dsm_tenant, timespan_from, timespan_to)
+    deep_security_usage_query(client, host, dsm_user, dsm_password, 0, dsm_tenant, timespan_from, timespan_to)
 
 if __name__ == '__main__':
     main()
